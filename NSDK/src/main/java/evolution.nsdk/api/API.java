0ee169e8a6736ecbd0916f8cc98310f7625c2d03
Jean de Leeuw <jeandeleeuw@gmail.com>
2019-05-10 17:33:57 +0200

    Update to point to remote endpoint

--- NSDK/src/main/java/evolution.nsdk/api/API.java
+++ NSDK/src/main/java/evolution.nsdk/api/API.java
@@ -2,7 +2,7 @@ package evolution.nsdk.api;
 
 public abstract class API {
 
-    protected final String baseURL = "http://localhost:8080";
+    protected final String baseURL = "https://jeansthesis.nl:8080";
     protected final int idKey;
 
     public API(int idKey) {
